3cd4d18351536cba9d55512fe1a9d241be9b0729
Igor <igordmoura.gomes@gmail.com.br>
2026-09-09 14:25:28 -0300

    Adiciona rota GET /pets

--- servidor.js
+++ servidor.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+let pets = [
+    { id: 1, nome: "Rex", especie: "Cachorro" },
+    { id: 2, nome: "Mimi", especie: "Gato" }
+];
 const PORTA = 3000;
 
 app.get('/', function(req, res) {
     res.send('Meu servidor PetShop está funcionando!');
 });
 
+app.get('/pets',function(req, res){
+    res.json(pets);
+});
+
 app.listen(PORTA, function() {
     console.log(`Servidor rodando em http://localhost:${PORTA}`);
 });
+
+
+

fb090e3f11751ac300f8360a6da7e36e6e196c29
Igor <igordmoura.gomes@gmail.com.br>
2026-09-11 18:39:02 -0300

    Adiciona rota POST / pets

--- servidor.js
+++ servidor.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
 let pets = [
     { id: 1, nome: "Rex", especie: "Cachorro" },
     { id: 2, nome: "Mimi", especie: "Gato" }
@@ -18,5 +20,10 @@ app.listen(PORTA, function() {
     console.log(`Servidor rodando em http://localhost:${PORTA}`);
 });
 
+app.post('/pets', function(req, res) {
+    let novoPet = req.body;
+    pets.push(novoPet);
+    res.json({ mensagem: "Pet adicionado com sucesso!", pet: novoPet });
+});
 
 

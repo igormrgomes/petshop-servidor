84c1aa811dd18a84ec9265e7049e7b7cef37c35d
Igor <igordmoura.gomes@gmail.com.br>
2026-09-21 20:32:01 -0300

    Rota GET /pets agora busca os dados no MongoDB Atlas

--- servidor.js
+++ servidor.js
@@ -10,10 +10,11 @@ app.get('/', function(req, res) {
     res.send('Meu servidor PetShop está funcionando!');
 });
 
-app.get('/pets', function(req, res) {
-    let textoLido = fs.readFileSync('dados.json', 'utf-8');
-    let pets = JSON.parse(textoLido);
-    res.json(pets);
+app.get('/pets', async function(req, res) {
+    // O await espera a busca na nuvem terminar
+    const pets = await colecaoPets.find({}).toArray(); 
+    // O driver já entrega um array pronto, sem precisar de JSON.parse!
+    res.json(pets); 
 });
 
 function gerarNovoId(listaDePets) {
@@ -70,3 +71,20 @@ app.get('/sobre', function(req, res) {
 app.listen(PORTA, function() {
     console.log(`Servidor rodando em http://localhost:${PORTA}`);
 });
+
+const { MongoClient } = require('mongodb');
+
+// Use a SUA connection string que funcionou na Aula 9.1
+const connectionString = "mongodb+srv://igordmouragomes_db_user:o4Dk1UVRomORdWnQ@cluster0.eepmjtz.mongodb.net/?appName=Cluster0"; 
+const client = new MongoClient(connectionString);
+
+let colecaoPets;
+
+async function conectarBanco() {
+    await client.connect();
+    const banco = client.db('petshop');
+    colecaoPets = banco.collection('pets');
+    console.log("Conectado à coleção de pets!");
+}
+
+conectarBanco();
